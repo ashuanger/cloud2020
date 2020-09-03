295dc42e0d6519c0c746e913a04618a1872c104f
shuangzhe.xie <602812514@qq.com>
2020-09-03 08:40:06 +0800

    新增nacos微服务

---
+++ cloudalibaba-provider-payment8020/src/main/java/com/xsz/springcloud/cloudalibabaProvider8020.java
@@ -0,0 +1,17 @@
+package com.xsz.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class cloudalibabaProvider8020 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(cloudalibabaProvider8020.class,args);
+    }
+
+
+
+}
---
+++ cloudalibaba-provider-payment8021/src/main/java/com/xsz/springcloud/cloudalibabaProvider8021.java
@@ -0,0 +1,15 @@
+package com.xsz.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class cloudalibabaProvider8021 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(cloudalibabaProvider8021.class,args);
+    }
+
+}
