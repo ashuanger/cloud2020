a1fb49830d2be5b9e3b707dea09ad955a73ece22
shuangzhe.xie <shuangzhe.xie@lanwon.com>
2020-09-09 16:31:13 +0800

    新增sentinel 监控中心

---
+++ cloudalibaba-sentinel-service-8401/src/main/java/com/xsz/springcloud/cloudalibabaSentinelService8404.java
@@ -0,0 +1,15 @@
+package com.xsz.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class cloudalibabaSentinelService8404 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(cloudalibabaSentinelService8404.class,args);
+    }
+
+}
