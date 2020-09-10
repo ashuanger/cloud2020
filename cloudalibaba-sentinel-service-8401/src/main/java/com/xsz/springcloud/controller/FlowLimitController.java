8e3c0763253f860ba58ef76b461ae63ac5b2216a
shuangzhe.xie <shuangzhe.xie@lanwon.com>
2020-09-10 17:32:59 +0800

    新增链路调用接口。

--- cloudalibaba-sentinel-service-8401/src/main/java/com/xsz/springcloud/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service-8401/src/main/java/com/xsz/springcloud/controller/FlowLimitController.java
@@ -1,9 +1,12 @@
 package com.xsz.springcloud.controller;
 
 
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @Slf4j
@@ -18,7 +21,19 @@ public class FlowLimitController {
     @GetMapping("/testB")
     public String testB()
     {
-//        log.info(Thread.currentThread().getName()+"\t"+"...testB");
+
+        System.out.println(Thread.currentThread().getName()+"\t"+"...testB");
+
         return "------testB";
     }
+
+    @GetMapping("/testD")
+    public String testD()
+    {
+        try { TimeUnit.SECONDS.sleep(1); } catch (InterruptedException e) { e.printStackTrace(); }
+        System.out.println("testD 测试RT");
+//        int age = 10/0;
+        return "------testD";
+    }
+
 }
